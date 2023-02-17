5c6e3c9d28deef5e02859eee39ab984dbe6ef301
moqiye <qiye.mo@foxmail.com>
2023-02-17 23:01:38 +0800

    fix:初始化代码

---
+++ src/main/java/com/mo/moadmin/MoadminApplication.java
@@ -0,0 +1,13 @@
+package com.mo.moadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoadminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mo/moadmin/MoadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mo.moadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
