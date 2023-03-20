7add11706d2c4cc4d0d6836e928862389e139cb0
qiyemo <qiye.mo@outlook.com>
2023-03-20 23:04:48 +0800

    fix:添加springdoc

--- src/main/java/com/mo/admin/controller/system/LoginController.java
+++ src/main/java/com/mo/admin/controller/system/LoginController.java
@@ -1,13 +1,24 @@
 package com.mo.admin.controller.system;
 
 import com.mo.admin.dto.Result;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(
+        name = "登录",
+        description = "登录相关 API"
+)
 @RestController
 @RequestMapping("system")
 public class LoginController {
+
+    @Operation(
+            summary = "用户登录",
+            description = "通过用户名，密码登录"
+    )
     @GetMapping("login")
     public Result<String> login(){
         return Result.success();
--- src/main/java/com/mo/admin/entity/User.java
+++ src/main/java/com/mo/admin/entity/User.java
@@ -1,23 +1,46 @@
 package com.mo.admin.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import org.hibernate.annotations.Table;
 
 import java.io.Serializable;
 
+@Schema(
+        description = "User Model Information"
+)
 @Data
 @Table(appliesTo = "sys_user")
 @Entity(name ="sys_user")
 public class User implements Serializable {
+
+    @Schema(
+            description = "用户唯一标识"
+    )
     @Id
     @Column(name="id")
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+
+    @Schema(
+            description = "用户名"
+    )
+    @NotEmpty(message = "用户名不能为空")
     @Column(name="username")
     private String username;
+
+
+    @Schema(
+            description = "电子邮件"
+    )
+    @NotEmpty(message = "电子邮件并不能为空")
+    @Email(message = "电子邮件并不能为空")
     @Column(name="email")
     private String email;
+
     @Column(name="phone")
     private String phone;
 }
